34c0b79bc6db8ca7a6838bbd0b2832c34df7e607
rowlinson <johk2293@gmail.com>
2022-10-30 17:00:33 +0400

    Add infinite load to user profile for collections

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {
   Container,
   Grid,
@@ -9,6 +9,7 @@ import {
   CardMedia,
   CardContent,
   CardActionArea,
+  Button,
   Typography,
   Divider,
 } from '@mui/material'
@@ -16,10 +17,14 @@ import { Image } from 'mui-image'
 import { useParams, useNavigate } from 'react-router-dom'
 import moment from 'moment'
 import { UserContext } from '../../context/UserContext'
-import getUserProfileData from '../../services/getUserProfileData'
+import {
+  getUserProfileData,
+  getUserCollections,
+} from '../../services/getUserProfileData'
 import Loader from '../Loader/Loader'
 import routes from '../../constants/routes'
 import { Trans, t } from '@lingui/macro'
+import { useInfiniteQuery } from 'react-query'
 
 function UserProfile() {
   const { userId } = useParams()
@@ -27,6 +32,15 @@ function UserProfile() {
   const [userData, setUserData] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const navigate = useNavigate()
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery(
+      ['userProfile'],
+      getUserCollections(userId ? userId : user.id),
+      {
+        getNextPageParam: (lastPage) =>
+          lastPage.collections.length < 8 ? undefined : lastPage.cursor,
+      }
+    )
 
   useEffect(() => {
     const fetchData = async () => {
@@ -80,7 +94,7 @@ function UserProfile() {
                   <Typography variant="body1">
                     <Trans>
                       <strong>Collections added:</strong>{' '}
-                      {userData.collections.length}
+                      {userData._count.collections}
                     </Trans>
                   </Typography>
                   <Typography variant="body1">
@@ -113,58 +127,97 @@ function UserProfile() {
                 item
                 container
                 xs={12}
-                spacing={2}
-                sx={{ mb: 3, alignItems: 'stretch' }}
+                spacing={1}
+                sx={{ alignItems: 'stretch' }}
               >
-                {userData.collections.map((el, index) => {
-                  return (
+                {status === 'loading' ? (
+                  <Loader />
+                ) : (
+                  <>
+                    {data.pages.map((group, i) => (
+                      <React.Fragment key={i}>
+                        {group.collections.map((item, index) => (
+                          <Grid
+                            item
+                            xs={12}
+                            sm={6}
+                            md={4}
+                            lg={3}
+                            key={`collection-${index}`}
+                            sx={{ display: 'flex' }}
+                          >
+                            <CardActionArea
+                              onClick={() => {
+                                navigate(
+                                  `${routes.COLLECTIONS}/byid/${item.id}`
+                                )
+                              }}
+                            >
+                              <Card
+                                sx={{
+                                  width: '100%',
+                                  height: '100%',
+                                  borderRadius: 0,
+                                }}
+                              >
+                                <CardMedia
+                                  component="img"
+                                  sx={{ height: { xs: 300, sm: 250, md: 200 } }}
+                                  image={
+                                    item.img ||
+                                    `https://via.placeholder.com/300?text=${t`No+image`}`
+                                  }
+                                  alt=""
+                                />
+                                <CardContent>
+                                  <Typography
+                                    variant="body1"
+                                    sx={{ fontWeight: 500 }}
+                                    component="div"
+                                  >
+                                    {item.name}
+                                  </Typography>
+                                  <Typography
+                                    variant="overline"
+                                    color="text.secondary"
+                                  >
+                                    {item.type.name}
+                                  </Typography>
+                                  <Typography
+                                    variant="body2"
+                                    color="text.secondary"
+                                  >
+                                    {moment(item.createdAt).format('LL')}
+                                  </Typography>
+                                </CardContent>
+                              </Card>
+                            </CardActionArea>
+                          </Grid>
+                        ))}
+                      </React.Fragment>
+                    ))}
                     <Grid
                       item
                       xs={12}
-                      sm={6}
-                      md={4}
-                      lg={3}
-                      key={`collection-${index}`}
-                      sx={{ display: 'flex' }}
+                      sx={{ display: 'flex', justifyContent: 'center' }}
                     >
-                      <CardActionArea
-                        onClick={() => {
-                          navigate(`${routes.COLLECTIONS}/byid/${el.id}`)
-                        }}
+                      <Button
+                        size="large"
+                        variant="outlined"
+                        color="inherit"
+                        onClick={() => fetchNextPage()}
+                        disabled={isFetchingNextPage}
+                        sx={{ display: !hasNextPage ? 'none' : 'block' }}
                       >
-                        <Card sx={{ width: '100%', height: '100%' }}>
-                          <CardMedia
-                            component="img"
-                            sx={{ height: { xs: 250, sm: 200, md: 150 } }}
-                            image={
-                              el.img ||
-                              `https://via.placeholder.com/300?text=${t`No+image`}`
-                            }
-                            alt=""
-                          />
-                          <CardContent>
-                            <Typography
-                              variant="body1"
-                              sx={{ fontWeight: 500 }}
-                              component="div"
-                            >
-                              {el.name}
-                            </Typography>
-                            <Typography
-                              variant="overline"
-                              color="text.secondary"
-                            >
-                              {el.type.name}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                              {moment(el.createdAt).format('LL')}
-                            </Typography>
-                          </CardContent>
-                        </Card>
-                      </CardActionArea>
+                        {isFetchingNextPage
+                          ? 'Loading more...'
+                          : hasNextPage
+                          ? 'Load More'
+                          : 'Nothing more to load'}
+                      </Button>
                     </Grid>
-                  )
-                })}
+                  </>
+                )}
               </Grid>
             </Grid>
           </Paper>
--- src/services/getUserProfileData.js
+++ src/services/getUserProfileData.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
 
-async function getUserProfileData(id) {
+export async function getUserProfileData(id) {
   const response = await axios.get('/users/profile', { params: { id } })
   return response.data
+}
+
+export function getUserCollections(id) {
+  const fetchData = async ({ pageParam = 1 }) => {
+    const response = await axios.get(`/collections/userCollections/${id}`, {
+      params: { page: pageParam },
+    })
+    return response.data
+  }
+  return fetchData
 }
 
 export default getUserProfileData
