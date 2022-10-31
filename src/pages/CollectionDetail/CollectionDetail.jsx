6f5332cf7a53d3c32020d0191ffdfd2242e408a2
rowlinson <johk2293@gmail.com>
2022-10-31 11:02:15 +0400

    Flx bug with data being undefined in collection detail

--- src/pages/CollectionDetail/CollectionDetail.jsx
+++ src/pages/CollectionDetail/CollectionDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState, useMemo } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getCollection } from '../../services/fetchCollections'
 import { Container, Typography, Grid, Box, Divider, Paper } from '@mui/material'
@@ -7,7 +7,7 @@ import ItemTable from './components/ItemTable'
 import Loader from '../../shared/Loader'
 import './CollectionDetail.css'
 import { UserContext } from '../../context/UserContext'
-import { useNavigate, Link } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import routes from '../../constants/routes'
 import Image from 'mui-image'
@@ -21,35 +21,21 @@ function CollectionDetail() {
   const { user } = useContext(UserContext)
   const { id } = useParams()
   const [rightToEdit, setRightToEdit] = useState(false)
-  const navigate = useNavigate()
-  const {
-    data: { collection, status },
-    isLoading,
-    refetch,
-  } = useQuery(['collectionDetail'], getCollection(id))
-
-  const defaultValues = useMemo(
-    () => ({
-      name: collection.name,
-      description: collection.description,
-    }),
-    [collection]
+  const { data, isLoading, refetch } = useQuery(
+    ['collectionDetail'],
+    getCollection(id)
   )
 
   useEffect(() => {
-    if (status === 'ok') {
-      setRightToEdit(
-        user.role === 'ADMIN' ||
-          (user.role === 'USER' && user.id === collection.authorId)
-      )
-    } else if (status === 'error') {
-      navigate(routes.NOT_FOUND, {
-        state: {
-          message: t`Collection not found`,
-        },
-      })
+    if (data) {
+      if (data.status === 'ok') {
+        setRightToEdit(
+          user.role === 'ADMIN' ||
+            (user.role === 'USER' && user.id === data.collection.authorId)
+        )
+      }
     }
-  }, [status, collection, user, navigate])
+  }, [data, user])
 
   return (
     <Container maxWidth="lg">
@@ -57,113 +43,129 @@ function CollectionDetail() {
         <Loader />
       ) : (
         <>
-          <CollectionContext.Provider
-            value={{
-              defaultValues,
-              refetch,
-            }}
-          >
-            <Grid container spacing={2}>
-              {collection.img ? (
-                <>
-                  <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ position: 'sticky', top: 20 }}>
-                      <Image src={collection.img} />
-                    </Box>
-                  </Grid>
-                </>
-              ) : (
-                ''
-              )}
-              <Grid
-                item
-                xs={12}
-                sm={collection.img ? 6 : 12}
-                md={collection.img ? 8 : 12}
-              >
-                <Paper sx={{ p: 2, position: 'sticky', top: 20 }}>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      width: '100%',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <Box>
-                      <Typography
-                        variant="subtitle1"
-                        component={Link}
-                        to={`${routes.USER_PROFILE}/${collection.author.id}`}
-                        sx={{
-                          mr: 2,
-                          textDecoration: 'none',
-                          color: 'text.primary',
-                          '&:hover': {
-                            textDecoration: 'underline',
-                          },
-                        }}
-                      >
-                        <Trans>Author: {collection.author.username}</Trans>
-                      </Typography>
-                      <Typography
-                        variant="caption"
-                        sx={{ color: 'text.secondary' }}
-                      >
-                        {moment(collection.createdAt).format('LL')}
-                      </Typography>
-                    </Box>
-                    <Box>
-                      <CollectionMenu
-                        rightToEdit={rightToEdit}
-                        id={collection.id}
-                      />
-                    </Box>
-                  </Box>
-                  <Typography
-                    variant="h5"
-                    sx={{ fontWeight: 500, textAlign: 'center' }}
-                  >
-                    {collection.name}
-                  </Typography>
-
-                  <Box sx={{ position: 'sticky', top: 20 }}>
-                    <Typography
-                      variant="overline"
-                      textAlign="center"
+          {data.status === 'ok' ? (
+            <CollectionContext.Provider
+              value={{
+                defaultValues: {
+                  name: data.collection.name,
+                  description: data.collection.description,
+                },
+                refetch,
+              }}
+            >
+              <Grid container spacing={2}>
+                {data.collection.img ? (
+                  <>
+                    <Grid item xs={12} sm={6} md={4}>
+                      <Box sx={{ position: 'sticky', top: 20 }}>
+                        <Image src={data.collection.img} />
+                      </Box>
+                    </Grid>
+                  </>
+                ) : (
+                  ''
+                )}
+                <Grid
+                  item
+                  xs={12}
+                  sm={data.collection.img ? 6 : 12}
+                  md={data.collection.img ? 8 : 12}
+                >
+                  <Paper sx={{ p: 2, position: 'sticky', top: 20 }}>
+                    <Box
                       sx={{
-                        display: 'block',
-                        textDecoration: 'none',
-                        color: 'text.primary',
+                        display: 'flex',
+                        width: '100%',
+                        justifyContent: 'space-between',
                       }}
                     >
-                      {collection.type.name}
+                      <Box>
+                        <Typography
+                          variant="subtitle1"
+                          component={Link}
+                          to={`${routes.USER_PROFILE}/${data.collection.author.id}`}
+                          sx={{
+                            mr: 2,
+                            textDecoration: 'none',
+                            color: 'text.primary',
+                            '&:hover': {
+                              textDecoration: 'underline',
+                            },
+                          }}
+                        >
+                          <Trans>
+                            Author: {data.collection.author.username}
+                          </Trans>
+                        </Typography>
+                        <Typography
+                          variant="caption"
+                          sx={{ color: 'text.secondary' }}
+                        >
+                          {moment(data.collection.createdAt).format('LL')}
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <CollectionMenu
+                          rightToEdit={rightToEdit}
+                          id={data.collection.id}
+                        />
+                      </Box>
+                    </Box>
+                    <Typography
+                      variant="h5"
+                      sx={{ fontWeight: 500, textAlign: 'center' }}
+                    >
+                      {data.collection.name}
                     </Typography>
-                    <Divider sx={{ my: 1 }} />
-                    <ReactMarkdown>{collection.description}</ReactMarkdown>
-                  </Box>
-                </Paper>
-              </Grid>
 
-              <Grid item xs={12}>
-                {collection.items && collection.items.length ? (
-                  <ItemTable
-                    items={collection.items}
-                    rightToEdit={rightToEdit}
-                  />
-                ) : (
-                  <Typography variant="body1">
-                    <em>
-                      <Trans>No items in collection</Trans>
-                    </em>
-                  </Typography>
-                )}
+                    <Box sx={{ position: 'sticky', top: 20 }}>
+                      <Typography
+                        variant="overline"
+                        textAlign="center"
+                        sx={{
+                          display: 'block',
+                          textDecoration: 'none',
+                          color: 'text.primary',
+                        }}
+                      >
+                        {data.collection.type.name}
+                      </Typography>
+                      <Divider sx={{ my: 1 }} />
+                      <ReactMarkdown>
+                        {data.collection.description}
+                      </ReactMarkdown>
+                    </Box>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12}>
+                  {data.collection.items && data.collection.items.length ? (
+                    <ItemTable
+                      items={data.collection.items}
+                      rightToEdit={rightToEdit}
+                    />
+                  ) : (
+                    <Typography variant="body1">
+                      <em>
+                        <Trans>No items in collection</Trans>
+                      </em>
+                    </Typography>
+                  )}
+                </Grid>
+                <AddItemForm
+                  rightToEdit={rightToEdit}
+                  collectionName={data.collection.name}
+                />
               </Grid>
-              <AddItemForm
-                rightToEdit={rightToEdit}
-                collectionName={collection.name}
-              />
-            </Grid>
-          </CollectionContext.Provider>
+            </CollectionContext.Provider>
+          ) : (
+            <Navigate
+              to={routes.NOT_FOUND}
+              state={{
+                message: t`Collection not found`,
+              }}
+            />
+          )}
         </>
       )}
     </Container>
